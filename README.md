959d55fe78b967f852aa4d72bb05950f1737f63c
Daniel9991 <35714347+Daniel9991@users.noreply.github.com>
2023-12-18 22:34:35 -0500

    Initial commit. Project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Hangman game developed in React
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='page'>
+      <header className="header">
+        <div className="invisible"></div>
+        <h1 className='heading'>Hangman</h1>
+        <a className='header-login' href="">Log In</a>
+      </header>
+      <section>
+        <p>Decipher the code</p>
+        <p>Save this poor man's life</p>
+      </section>
+      
+      <footer>
+        <h3>Classic Hangman</h3>
+        <section>
+
+        </section>
+        <span>Hangman &copy; 2023</span>
+      </footer>
+    </div>
+  )
+}
+
+export default App
